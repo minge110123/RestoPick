9c5c08798349c4272ae486c11c1382ad3e124542
minge110123 <87860920+minge110123@users.noreply.github.com>
2023-06-23 22:33:34 +0900

    Initial Commit

---
+++ RestoPick/RestoPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestoPickApp.swift
+//  RestoPick
+//
+//  Created by ZML on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RestoPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
